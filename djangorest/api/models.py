3a839357c64836614f83fad4618444939618e338
malisaroj <malisaroj@icloud.com>
2020-05-02 15:44:55 +0545

    add post and comment class in model

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     user_email = models.EmailField(max_length=254)
     name = models.CharField(max_length=255, blank=False)
     gender = models.CharField(max_length=8, blank=False)
-    dob = models.DateField(blank=False)
+    dob = models.DateTimeField(blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -15,11 +15,25 @@ class User(models.Model):
         return "{} {} {}".format(self.username, self.name, self.gender)
 
 class Post(models.Model):
-    """This class represents the post model."""
-    caption = models.CharField(max_length=255)
-    user_id = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
+    """This class represents the Post model."""
+    image = models.ImageField
+    caption = models.TextField(max_length=255)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
+ 
+class Comment(models.Model):
+    """This class represents the Comment Model."""
+    comment = models.TextField(max_length=255)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.comment)
